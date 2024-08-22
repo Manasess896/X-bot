968c5863bcc982bf6e058680464cae60f2bf7b7a
Manases <147276750+Manasess896@users.noreply.github.com>
2024-08-22 19:57:51 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -104,9 +104,7 @@ class WeatherBot:
             )
 
 
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+
 
 
  
